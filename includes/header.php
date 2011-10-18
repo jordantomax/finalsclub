e913ee17a661a7959ccf58f5cc20ee25e5c36549
Jordan Cooperman <jordancooperman@gmail.com>
2011-10-17 20:49:12 -0400

    removed unused files

--- includes/header.php
+++
@@ -1,8 +0,0 @@
-<div id="header">
-	<div class="container-<?= $columns ?>">
-	<div id="logo">
-	    <a id="logo" href="/" alt=""></a>
-	</div>
-	<?php include("includes/navigation.php"); ?>
-
-</div>
